7ae64202b793c54cd379a64cc8963991a4de8008
jordan <jordan@AESD.myguest.virtualbox.org>
2026-09-14 11:21:36 -0700

    Updated project to support custom kernel driver modules

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,20 +6,36 @@
 ##############################################################
 
 # TODO: Update with assignment 7 git repo hash
-LDD_VERSION = '5c3cae6ddc96b8645dfa6f6bc4ddbba08aae8789'
+LDD_VERSION = 'af5b19f0f542cc433c58a13d7b95c19636820013'
 LDD_SITE = 'git@github.com:scotch115/aeld-assignment-7.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules/
+LDD_MODULE_SUBDIRS += scull/
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 # TODO: Update build comments
 define LDD_BUILD_CMDS
-	# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules modules
+	$(MAKE) -C $(@D)/misc-modules
+	$(MAKE) -C $(@D)/scull
+	# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
+	# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
 endef
 
 # TODO Update install commands below to align with what is needed to install LDD
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/* $(TARGET_DIR)/etc/misc_modules
-	$(INSTALL) -m 0755 $(@D)/* $(TARGET_DIR)/etc/misc_modules
+	$(INSTALL) -d 0755 $(@D)/misc-modules/ $(TARGET_DIR)/etc/misc-modules
+	$(INSTALL) -d 0755 $(@D)/scull/ $(TARGET_DIR)/etc/scull
+
+	$(INSTALL) -m 0755 $(@D)/misc-modules/*.ko $(TARGET_DIR)/etc/misc-modules
+	$(INSTALL) -m 0755 $(@D)/misc-modules/*.o $(TARGET_DIR)/etc/misc-modules
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/etc/misc-modules
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/etc/misc-modules
+	
+	$(INSTALL) -m 0755 $(@D)/scull/*.ko $(TARGET_DIR)/etc/scull
+	$(INSTALL) -m 0755 $(@D)/scull/*.o $(TARGET_DIR)/etc/scull
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/etc/scull
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/etc/scull
 endef
+$(eval $(kernel-module))
 $(eval $(generic-package))
